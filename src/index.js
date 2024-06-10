5c26564f4d1acf36b6e159f0a4a8af281f174403
lezernack <luks.ezck@gmail.com>
2024-06-10 15:07:32 -0600

    added code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { configureStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//STORE -> GLOBALIZED STATE
+
+//ACTION INCREMENT
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+//REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+let store = configureStore(counter);
+
+//Display it in the console
+store.subscribe(() => console.log(store.getState()));
+
+//DISPATCH
+store.dispatch(increment);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
