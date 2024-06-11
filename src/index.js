ed80c5d29383e7e35982869c3e3be8e60966809b
lezernack <luks.ezck@gmail.com>
2024-06-11 14:30:49 -0600

    changed hello to counter

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { legacy_createStore } from "redux";
 import allReducers from "./reducers";
+import { Provider } from "react-redux";
 
 const store = legacy_createStore(
   allReducers,
@@ -14,7 +15,9 @@ const store = legacy_createStore(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
