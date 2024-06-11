8e25bb4e930ad0ea444caa074449f7536a41b5f2
lezernack <luks.ezck@gmail.com>
2024-06-11 15:09:29 -0600

    finished video code(working on styling)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import "./index.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
 
 function App() {
   const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
+      <div className="btn">
+        <button onClick={() => dispatch(increment(3))}>+</button>
+        <button onClick={() => dispatch(decrement(3))}>-</button>
+      </div>
+
+      {isLogged ? <h3>Valuable Information I shouldn't see</h3> : ""}
     </div>
   );
 }
