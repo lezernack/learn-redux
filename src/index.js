e1bc31b13684bc49c44f3bb538a3621d8e70d4c6
lezernack <luks.ezck@gmail.com>
2024-06-11 14:08:36 -0600

    added redux-devtools

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import reportWebVitals from "./reportWebVitals";
 import { legacy_createStore } from "redux";
 import allReducers from "./reducers";
 
-const store = legacy_createStore(allReducers);
+const store = legacy_createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
